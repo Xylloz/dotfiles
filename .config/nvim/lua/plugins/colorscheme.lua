16545a8de8d6be923a4fb996783a1e95ec654bc8
xylloz <tehxyllo@gmail.com>
2024-09-26 03:47:08 -0400

    Added plugins

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        vim.cmd.colorscheme("tokyonight")
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		require("tokyonight").setup({
+			transparent = true,
+		})
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,7 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	opts = {},
+	config = function()
+		require("ibl").setup()
+	end,
+}
